e824ab312db09983c41b6ed4d6df88f9bfaf4067
Bao (Brian) L. LE <lelongbao2790@gmail.com>
2016-01-05 09:12:02 +0700

    - Create source code for app.

---
+++ ProtoEditor/ProtoEditor/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ProtoEditor
+//
+//  Created by Bao (Brian) L. LE on 1/5/16.
+//  Copyright © 2016 Brian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ProtoEditor/ProtoEditor/Controller/MainController/MainController.h
@@ -0,0 +1,13 @@
+//
+//  MainController.h
+//  ProtoEditor
+//
+//  Created by Bao (Brian) L. LE on 1/5/16.
+//  Copyright © 2016 Brian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainController : UIViewController
+
+@end
